1bd33839c2221969284a1b4a65e1f700b7ac544f
faturfawkes <nur.faturohman28@gmail.com>
2023-09-10 22:26:32 +0700

    data type js

---
+++ javascript/dataType.js
@@ -0,0 +1,25 @@
+// console.log("Hello World")
+
+// LET
+// let strVar = "Hello World"
+// strVar = "Hello World var"
+// let intVar = 12345
+// console.log(strVar) 
+// console.log(intVar) 
+
+// CONST
+// const strVar = "Hello World"
+// console.log(strVar)
+
+// Length
+// let name = "fatur rohman"
+// let length = name.length
+// console.log(length)
+
+// Number
+let number = 123
+let float = 12.3
+
+let numberA = 20
+let numberB = 20
+console.log("Result of numberA and B : ", numberA + numberB)
