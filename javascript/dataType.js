a34461f9801d503e667d1ca61c520e36444f1d70
faturfawkes <nur.faturohman28@gmail.com>
2023-09-11 00:14:13 +0700

    object and array

--- javascript/dataType.js
+++ javascript/dataType.js
@@ -17,9 +17,50 @@
 // console.log(length)
 
 // Number
-let number = 123
-let float = 12.3
+// let number = 123
+// let float = 12.3
 
-let numberA = 20
-let numberB = 20
-console.log("Result of numberA and B : ", numberA + numberB)
+// let numberA = 20
+// let numberB = 20
+// console.log("Result of numberA and B : ", numuberA + numberB)
+
+// Null / Undifined
+// let undifinedVar // tidak ada value
+// let nullVar = null 
+// console.log("hasil undifined : ", undifinedVar)
+// console.log("hasil nullVar : ", nullVar)
+
+// Object
+// let person = {
+//     name : "Nur Fatchurohman",
+//     age: 21,
+//     address: {
+//         branch: "Jonggol",
+//         region: "Bogor",
+//     }
+// }
+// console.log(person.address)
+
+// Array
+// let array = ["fatur", 21, person]
+// console.log(array)
+
+// Cek Tipe Data
+// let varStr = "lorem ipsum dolor sit amet"
+// let varInt = 1234
+// let undifined
+// let varNull = null
+// let varObj = {}
+// let varArr = []
+// console.log("varStr", typeof varStr)
+// console.log("varInt", typeof varInt)
+// console.log("var undefined", typeof undifined)
+// console.log("varNull", typeof varNull)
+// console.log("varObj", typeof varObj)
+// console.log("varArr", typeof varArr)
+
+// Reassignment
+// let numA = "10"
+// let numB = 10
+// console.log(numA === numB) // false
+
