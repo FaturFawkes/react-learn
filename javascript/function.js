af049ee3d0cc878c3493f800c1b64c5ddced6749
faturfawkes <nur.faturohman28@gmail.com>
2023-09-11 20:30:18 +0700

    function

---
+++ javascript/function.js
@@ -0,0 +1,15 @@
+// function sumNumber(numC) {
+//   const numberA = 10;
+//   const numberB = 20;
+//   console.log(numberA + numberB + numC);
+// }
+
+// sumNumber(); // NaN
+// sumNumber(5); // 25
+
+// // Function with return
+// function sumNumberWithRetrun(numA, numB) {
+//   const sum = numA + numB;
+//   return sum;
+// }
+// console.log(sumNumberWithRetrun(5, 10))
