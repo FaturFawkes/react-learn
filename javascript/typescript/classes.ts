b922a0aad2b721430d62a04f9ea3c7aecb76d9c2
faturfawkes <nur.faturohman28@gmail.com>
2023-09-16 01:30:57 +0700

    interface property

--- javascript/typescript/classes.ts
+++ javascript/typescript/classes.ts
@@ -18,23 +18,50 @@ class Fruit {
   color: string;
 
   constructor(color: string) {
-    this.color = color
+    this.color = color;
   }
 }
 
 class Banana extends Fruit {
-  price: number
+  price: number;
 
-  constructor(price: number, color: string,) {
-    super(color)
-    this.price = price
+  constructor(price: number, color: string) {
+    super(color);
+    this.price = price;
   }
 
   display(): void {
-    console.log("Color of Banana fruit is :", this.color)
-    console.log("Price of Banana fruit is :", this.price)
+    console.log("Color of Banana fruit is :", this.color);
+    console.log("Price of Banana fruit is :", this.price);
   }
 }
 
 let fruitObj = new Banana(4000, "Yellow");
-fruitObj.display()
+// fruitObj.display()
+
+// Interface
+interface Person {
+  firstName: string;
+  lastName: string;
+  age?: number;
+  getFullName(): string;
+}
+
+class PersonImpl {
+  firstName: string
+  lastName: string
+  age?: number
+  
+  constructor(firstName: string, lastName: string, age?: number) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+  }
+  
+  getFullName(): string {
+    return this.firstName + this.lastName
+  } 
+} 
+
+const person1: Person = new PersonImpl("Nur", "Fatchurohman")
+console.log(person1.getFullName())
